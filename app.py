16abf8371187dd443f8813ec467958889c48dd53
SARA3SAEED <Swaretoo@gmail.com>
2024-10-03 19:07:09 +0300

    2 commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import time
-
+import os
 import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+# cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(host=os.environ.get('REDIS_HOST'), port=6379)
+
 
 def get_hit_count():
     retries = 5
